becd951febca314878b2a0f388c552aedd38c9d7
guicherm <Maceo.Guicherd-Callin@etu.univ-grenoble-alpes.fr>
2025-11-10 16:30:52 +0100

    lier le formulaire avec les autres pages (vu que l'extension du fichier a changé)

--- mail.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$retour = mail('maceo.guicherd@gmail.com', 'Message depuis le portfolio de '.$_POST['Nom'],$_POST['message'].' \n pour répondre : '.$_POST['Mail'], '');
-if ($retour){
-    echo '<p>Votre message a bien été envoyé.</php>';
-}
-?>
